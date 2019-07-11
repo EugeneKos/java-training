138fe7434e1ffd352c2305e059ace04606e44e0f
EugeneKos <kosinov4444@yandex.ru>
2019-07-11 18:00:32 +0300

    test simple case reflection api

--- reflection-api-training/src/main/java/ru/eugene/java/learn/App.java
+++ reflection-api-training/src/main/java/ru/eugene/java/learn/App.java
@@ -3,12 +3,16 @@ package ru.eugene.java.learn;
 import ru.eugene.java.learn.proxy.PersonProxyHandler;
 import ru.eugene.java.learn.proxy.PersonService;
 import ru.eugene.java.learn.proxy.PersonServiceImpl;
+import ru.eugene.java.learn.reflection.ComputerModel;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 public class App {
-    public static void main(String[] args) {
-        testPersonProxy();
+    public static void main(String[] args) throws Exception {
+        //testPersonProxy();
+        testReflectionComputerModel();
     }
 
     private static void testPersonProxy() {
@@ -21,5 +25,38 @@ public class App {
                         new PersonProxyHandler(personService));
 
         proxy.degradation();
+    }
+
+    private static void testReflectionComputerModel() throws Exception {
+        ComputerModel model = new ComputerModel("dell", 3, 16);
+        System.out.println(model);
+
+        Class modelClass = ComputerModel.class;
+        Method[] methods = modelClass.getDeclaredMethods();
+        Field[] fields = modelClass.getDeclaredFields();
+
+        Field field = null;
+        for (Field f : fields){
+            if("memorySize".equals(f.getName())){
+                field = f;
+            }
+        }
+        if(field != null){
+            field.setAccessible(true);
+            field.setInt(model, 32);
+        }
+        System.out.println(model);
+
+        Method method = null;
+        for (Method m : methods){
+            if("overclock".equals(m.getName())){
+                method = m;
+            }
+        }
+        if(method != null){
+            method.setAccessible(true);
+            method.invoke(model);
+        }
+        System.out.println(model);
     }
 }
---
+++ reflection-api-training/src/main/java/ru/eugene/java/learn/reflection/ComputerModel.java
@@ -0,0 +1,31 @@
+package ru.eugene.java.learn.reflection;
+
+public class ComputerModel {
+    private String fullName;
+    private double frequency;
+    private int memorySize;
+    public int aaa;
+
+    public ComputerModel(String fullName, double frequency, int memorySize) {
+        this.fullName = fullName;
+        this.frequency = frequency;
+        this.memorySize = memorySize;
+    }
+
+    public void powerOn(){
+        System.out.println("power on " + fullName);
+    }
+
+    private void overclock(){
+        frequency = frequency + 2;
+    }
+
+    @Override
+    public String toString() {
+        return "ComputerModel{" +
+                "fullName='" + fullName + '\'' +
+                ", frequency=" + frequency +
+                ", memorySize=" + memorySize +
+                '}';
+    }
+}
