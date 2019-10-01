d55906c430948096e4389dc04acfc925c0ac02cd
EugeneKos <kosinov4444@yandex.ru>
2019-10-01 17:09:25 +0300

    hibernate-orm-training: Added method in PersonService.

--- hibernate-orm-training/src/main/java/ru/eugene/java/learn/service/IPersonService.java
+++ hibernate-orm-training/src/main/java/ru/eugene/java/learn/service/IPersonService.java
@@ -5,5 +5,7 @@ import ru.eugene.java.learn.data.Person;
 import java.util.Set;
 
 public interface IPersonService {
+    Person getByCode(String code);
+
     Set<Person> getAllWithAutomobiles();
 }
--- hibernate-orm-training/src/main/java/ru/eugene/java/learn/service/impl/PersonServiceImpl.java
+++ hibernate-orm-training/src/main/java/ru/eugene/java/learn/service/impl/PersonServiceImpl.java
@@ -3,6 +3,7 @@ package ru.eugene.java.learn.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import ru.eugene.java.learn.data.Person;
 import ru.eugene.java.learn.repository.PersonRepository;
 import ru.eugene.java.learn.service.IPersonService;
@@ -16,6 +17,12 @@ public class PersonServiceImpl implements IPersonService {
     @Autowired
     public PersonServiceImpl(PersonRepository personRepository) {
         this.personRepository = personRepository;
+    }
+
+    @Override
+    @Transactional
+    public Person getByCode(String code) {
+        return personRepository.findByCode(code);
     }
 
     @Override
