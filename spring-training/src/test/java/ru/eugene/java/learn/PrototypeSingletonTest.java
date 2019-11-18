6b60737535bc0ec4648ed2a9a1b76ee1ec3fce32
EugeneKos <kosinov4444@yandex.ru>
2019-11-18 11:09:41 +0300

    spring-training: Added test for Prototype in Singleton example.

--- spring-training/src/test/java/ru/eugene/java/learn/PrototypeSingletonTest.java
+++ spring-training/src/test/java/ru/eugene/java/learn/PrototypeSingletonTest.java
@@ -1,13 +1,15 @@
 package ru.eugene.java.learn;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import ru.eugene.java.learn.config.SpringConfiguration;
 import ru.eugene.java.learn.data.Car;
 
-public class SpringMain {
-    public static void main(String[] args) {
+public class PrototypeSingletonTest {
+    @Test
+    public void prototypeSingletonTest(){
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfiguration.class);
         Car car = context.getBean(Car.class);
         for (int i = 0; i < 100; i++) {
