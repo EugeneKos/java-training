d61a75cf90a9ea2da4203acf508df94407311796
Eugene_Kosinov <kosinov4444@yandex.ru>
2019-12-08 12:36:37 +0300

    hibernate-orm-training: Added Group entity and GroupRepository and GroupDao and Tests.

---
+++ hibernate-orm-training/src/main/java/ru/eugene/java/learn/dao/GroupDao.java
@@ -0,0 +1,50 @@
+package ru.eugene.java.learn.dao;
+
+import ru.eugene.java.learn.data.Group;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityGraph;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Subgraph;
+import javax.persistence.TypedQuery;
+
+@Component
+public class GroupDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    public GroupDao(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Transactional
+    public Group findByNameWithLoadChildren(String name, int loadDeep){
+        if(loadDeep < 0){
+            throw new IllegalArgumentException("load deep don't must be negative");
+        }
+
+        TypedQuery<Group> query = entityManager.createQuery("select g from Group g where g.name = :name", Group.class);
+        query.setParameter("name", name);
+
+        if(loadDeep >= 1){
+            EntityGraph<Group> entityGraph = entityManager.createEntityGraph(Group.class);
+            entityGraph.addAttributeNodes("childrenGroups");
+
+            if(loadDeep > 1){
+                Subgraph<Object> childrenGroups = entityGraph.addSubgraph("childrenGroups");
+                for (int i = 1; i < loadDeep; i++) {
+                    childrenGroups.addAttributeNodes("childrenGroups");
+                    childrenGroups = childrenGroups.addSubgraph("childrenGroups");
+                }
+            }
+            query.setHint(org.springframework.data.jpa.repository.EntityGraph.EntityGraphType.FETCH.getKey(), entityGraph);
+        }
+
+        return query.getSingleResult();
+    }
+}
